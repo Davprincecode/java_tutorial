b8918504a10faa8f9fba4bb001e8ce0928f02fcf
bebesf <89807577+bebesf@users.noreply.github.com>
2026-07-27 15:40:00 +0100

    bankinga

--- src/main/java/com/example/lessontooth/assignment/BankA.java
+++ src/main/java/com/example/lessontooth/assignment/BankA.java
@@ -1,4 +1,25 @@
 package com.example.lessontooth.assignment;
 
 public class BankA {
+
+        String accountNumber;
+        double balance;
+
+        public BankA (String accountNumber, double initialBalance) {
+            this.accountNumber = accountNumber;
+            this.balance = initialBalance;
+        }
+
+        // Method 1: Deposit money (No conditions)
+        public void deposit(double amount) {
+            balance += amount;
+            System.out.println("Successfully deposited: $" + amount);
+        }
+
+        // Method 2: Withdraw money (No conditions)
+        public void withdraw(double amount) {
+            balance -= amount;
+            System.out.println("Successfully withdrew: $" + amount);
 }
+}
+
--- src/main/java/com/example/lessontooth/task4/BankAccount.java
+++ src/main/java/com/example/lessontooth/task4/BankAccount.java
@@ -1,6 +1,9 @@
+
 package com.example.lessontooth.task4;
+//Bank Account: Build a BankAccount class with a constructor for account number and balance, and methods for deposit, withdraw, and check balance.
 
 public class BankAccount {
+    
     public  void addAccount(){
       System.out.print("my acoount no");
     }
